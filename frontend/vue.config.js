063e1ac2ea8d1e7b4d07923f4bcc156ce076e5f1
Richard Algra <richard.b.algra@gmail.com>
2023-01-31 23:58:29 +0100

    Commit to test deployment to gitlab-pages

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
 })
+//
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/' + process.env.CI_PROJECT_NAME + '/'
+//     : '/'
+// }
