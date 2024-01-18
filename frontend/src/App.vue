1e020b79d0d267e237432edb75ac86277f2d2138
Richard Algra <Richard.b.algra@gmail.com>
2024-01-18 08:58:38 +0100

    Add deployment todo's

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,13 @@
+<!--Items to do before initial deployment: (https://richardalgra.com/) -->
+<!--TODO: Create landingSection ThreeJs scene, mind the issue of dynamic vue messing up static 3Js objects.-->
+<!--TODO: Fix landingSection text fading when scrolling away.-->
+<!--TODO: Cleanup landingSection vue file.-->
+<!--TODO: Cleanup aboutSection vue file.-->
+<!--TODO: Fix CSS of experienceSection.-->
+<!--TODO: Fix backend of contactSection.-->
+<!--TODO: Add footer element to contactSection.-->
+<!--TODO: Work on security, ex: properly resolve cors.-->
+
 <template>
   <router-view/>
 </template>
