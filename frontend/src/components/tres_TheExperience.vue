e55bd596f996f4a0914f9086fbd7f95a5d5cc4af
Richard Algra <Richard.b.algra@gmail.com>
2024-03-08 16:57:23 +0100

    Add sample animation progress

--- frontend/src/components/tres_TheExperience.vue
+++ frontend/src/components/tres_TheExperience.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
+import {useRenderLoop} from "@tresjs/core";
+import {ref} from "vue";
 
+const { onLoop } = useRenderLoop()
+const meshRef = ref('meshRef')
+
+onLoop(({ delta, elapsed }) => {
+  if (meshRef.value) {
+    // meshRef.value.rotation.y += delta
+    // meshRef.value.rotation.z = elapsed * 0.2
+  }
+})
 </script>
 <template>
     <TresCanvas>
       <TresPerspectiveCamera/>
       <TresScene>
-        <TresMesh>
+        <TresMesh ref="meshRef">
           <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
           <TresMeshBasicMaterial color="orange" />
           <TresAmbientLight :intensity="1" />
         </TresMesh>
       </TresScene>
+      <TresAxesHelper/>
     </TresCanvas>
 </template>
 
