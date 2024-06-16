3be0584d64519b4823564a4d49783618311e83e5
Richard Algra <richard.b.algra@gmail.com>
2024-06-16 20:49:19 +0200

    Projects: correct cube rotation behaviour

--- frontend/src/components/projects_tile.vue
+++ frontend/src/components/projects_tile.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 
 interface Project {
   thumbnail: string;
@@ -31,30 +31,51 @@ export default defineComponent({
       required: true,
     }
   },
-  methods: {
-    rotate() {
-      this.$el.style.transform = 'perspective(1200px) rotateY(-90deg)';
-    },
-    reset() {
-      this.$el.style.transform = 'perspective(2300px) rotateY(0deg)';
-    },
+  setup() {
+    const isMouseInside = ref(false);
+    const transformStyle = ref('perspective(1200px) rotateY(0deg)');
+    const handleMouseEnter = () => {
+      if (!isMouseInside.value) {
+        transformStyle.value = 'perspective(1200px) rotateY(-90deg)';
+        isMouseInside.value = true;
+      }
+    };
+    const handleMouseLeave = () => {
+      if (isMouseInside.value) {
+        transformStyle.value = 'perspective(1200px) rotateY(0deg)';
+        isMouseInside.value = false;
+      }
+    };
+    return {
+      transformStyle,
+      handleMouseEnter,
+      handleMouseLeave
+    };
   },
 });
 </script>
 
 <template>
-  <div class="cube" :style="{width: width, height: height, margin: margin}" @mouseover="rotate" @mouseout="reset">
-    <div class="face front" :style="{
-      width: width,
-      height: height,
-      transform: 'translateZ('+translate+')'}">
-      <img :src="'src/assets/'+project.thumbnail" :alt="project.title">
-    </div>
-    <div class="face right" :style="{width: width, height: height, transform: 'rotateY(90deg) translateZ('+translate+')'}">
-      <h3 class="title G_sectionHeader">{{ project.title }}</h3>
-      <p class="description">{{ project.description }}</p>
-      <div class="git-icon"/>
-      <div class="mini-icons-list"></div>
+  <div class="cube-container" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
+    <div
+        class="cube"
+        :style="{ width: width, height: height, margin: margin, transform: transformStyle }"
+    >
+      <div
+          class="face front"
+          :style="{ width: width, height: height, transform: 'translateZ(' + translate + ')' }"
+      >
+        <img :src="'src/assets/' + project.thumbnail" :alt="project.title">
+      </div>
+      <div
+          class="face right"
+          :style="{ width: width, height: height, transform: 'rotateY(90deg) translateZ(' + translate + ')' }"
+      >
+        <h3 class="title G_sectionHeader">{{ project.title }}</h3>
+        <p class="description">{{ project.description }}</p>
+        <div class="git-icon"></div>
+        <div class="mini-icons-list"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -62,24 +83,31 @@ export default defineComponent({
 <style scoped lang="scss">
 @import "../styles/theme";
 
+.cube-container {
+  position: relative;
+  display: inline-block;
+  width: fit-content;
+  height: fit-content;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    background: transparent;
+  }
+}
 .cube {
   position: relative;
   transform-style: preserve-3d;
-  transition: transform $rotation-speed;
+  transition: transform 0.4s;
   transform: perspective(1200px) rotateY(0deg);
-  &:hover {
-    transform: perspective(1200px) rotateY(-90deg);
-  }
-  &.no-perspective {
-    transform: perspective(2300px) rotateY(-90deg);
-  }
+  z-index: 0;
   .face {
     position: absolute;
-    background: linear-gradient(
-            30deg,
-            rgb(50,50,50),
-            rgb(100,100,100)
-    );
+    background: linear-gradient(30deg, rgb(50, 50, 50), rgb(100, 100, 100));
     backface-visibility: hidden;
     transform-style: flat;
     text-align: center;
